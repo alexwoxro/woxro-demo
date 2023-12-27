18bd7eb6940e36b9a0354f940de7ea87bc578909
Alex <alex.george@woxro.in>
2023-12-27 18:03:46 +0530

    update

--- components/home/hero/hero.js
+++ components/home/hero/hero.js
@@ -1,7 +1,9 @@
-import Image from 'next/image'
-import React from 'react'
 
+import React from 'react'
 import bg from '/assets/images/hero/hero-bg-image.png'
+
+import Image from 'next/image'
+import btn from '/assets/icons/button/btn-r-arrow.svg'
 import Button from '../button/button'
 
 const Hero = () => {
@@ -15,18 +17,28 @@ const Hero = () => {
 
         <div className="relative z-10 flex flex-col items-center justify-center h-full
          text-white px-10 ">
-          <p className='font-poppins text-xs font-medium leading-3  lg:leading-[18px]
-           '>CRAFTED FROM KERALA TO THE WORLD</p>
-          <h1 className='font-roboto font-thin text-2xl lg:text-[3.4rem] leading-3 
-          lg:leading-[64px] mt-5 lg:mt-3.5'>World-Class Digital Services</h1>
-          <div className='text-center font-poppins font-thin lg:font-medium leading-4  lg:leading-6 text-xs
-           lg:text-sm mt-5  lg:mt-3.5'>We specialize in crafting unforgettable brands that establish a profound connection <br />
+          <p className='font-poppins text-xs font-normal leading-[1.125rem] tracking-[2px]'>
+            CRAFTED FROM KERALA TO THE WORLD</p>
+          <h1 className='font-roboto font-thin text-2xl lg:text-[3.438rem] leading-3 lg:leading-[4rem] mt-5 lg:mt-3.5'>
+            World-Class Digital Services</h1>
+
+          <div className='text-center font-poppins font-thin lg:font-normal leading-4  lg:leading-[1.563rem] text-xs
+           lg:text-[0.938rem] mt-5  lg:mt-3.5'>We specialize in crafting unforgettable brands that establish a profound connection <br />
             with your audience, elevating customer relationships through a seamless blend of <br />
             strategic design and cutting-edge technology.</div>
-          <div className='mt-8'>
-            <Button>Schedule a Meeting</Button>
-          </div>
+
+
+
+          
+
+
+          <Button>Schedule a Meeting</Button>
+
+
+
+
         </div>
+
       </div >
     </>
   )
